79f4f4fe6e0a995d1492411f633d0c427c7d1667
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-12-08 16:14:40 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -76,12 +76,10 @@ class MyHouse extends House {
 Наприклад, ось так : */
 
 const key = new Key();
-
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 export {};
